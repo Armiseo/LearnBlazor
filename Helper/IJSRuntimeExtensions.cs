73569a9f07331bbba5635e68e10883ab0d4f5770
valea <valea@POLLUX-GAMING>
2026-04-08 11:05:26 +0200

    JsInterop : Adding toastr and sweet alert

---
+++ Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrNotification(this IJSRuntime jsRuntime, string type)
+        {
+            var message = "";
+            if (type == "success")
+            {
+                message = "Good Job Dude";
+            }
+            else if (type == "warning")
+            {
+                message = "Careful, maybe an error here";
+            }
+            else
+            {
+                message = "It's very wrong";
+            }
+
+            jsRuntime.InvokeVoidAsync("showToastr", type, message);
+        }
+
+        public static async Task SweetAlertNotification(this IJSRuntime jsRuntime, string type)
+        {
+            var message = "";
+            if (type == "success")
+            {
+                message = "Good Job Dude";
+            }
+            else
+            {
+                message = "It's very wrong";
+            }
+
+            jsRuntime.InvokeVoidAsync("showSweetAlert", type, message);
+        }
+    }
+}
---
+++ wwwroot/js/common.js
@@ -0,0 +1,30 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message)
+    }
+    else if (type == "warning") {
+        toastr.warning(message)
+    }
+    else {
+        toastr.error(message)
+    }
+}
+
+window.showSweetAlert = function (type, message) {
+    if (type == "success") {
+        Swal.fire({
+            icon: "success",
+            title: "Great Job",
+            text: message,
+        });
+    }
+    else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: message,
+            footer: "<a href=\"#\">Why do I have this issue?</a>"
+        });
+    }
+}
+
